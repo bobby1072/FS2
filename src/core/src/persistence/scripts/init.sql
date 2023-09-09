04285b1eabd765512cfafb92b3e29f63b9fefbfc
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2023-09-09 16:27:00 +0100

    Initial commit

--- src/core/src/persistence/scripts/init.sql
+++ src/core/src/persistence/scripts/init.sql
@@ -1,6 +1,25 @@
+CREATE TABLE public.permissions (
+    buzzword TEXT PRIMARY KEY
+);
+
+CREATE TABLE public.user_role (
+    role_name TEXT PRIMARY KEY,
+    affect_user TEXT NOT NULL,
+    CONSTRAINT fk_user_buzzword FOREIGN KEY (affect_user) REFERENCES public.permissions(buzzword),
+    affect_group TEXT NOT NULL,
+    CONSTRAINT fk_group_buzzword FOREIGN KEY (affect_group) REFERENCES public.permissions(buzzword),
+
+);
+
 CREATE TABLE public.user (
-    email TEXT PRIMARY KEY UNIQUE,
+    username TEXT PRIMARY KEY UNIQUE,
+    first_name TEXT NOT NULL,
+    description TEXT, 
+    verified BOOLEAN DEFAULT false,
+    email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
     phone_number TEXT,
-    created_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC')
+    created_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
+    role_name TEXT NOT NULL,
+    CONSTRAINT fk_role_name FOREIGN KEY (role_name) REFERENCES public.user_role(role_name)
 );
