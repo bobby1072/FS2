8252869aeb24b5b9f715f1a1c7710d442a4b127f
Bobby Thompson <bobby.thompson1072@gmail.com>
2024-06-20 12:10:19 +0100

    More pipeline changes

---
+++ add_build_env_frontend.ps1
@@ -0,0 +1,12 @@
+param (
+    [string]$backendUrl = "http://localhost:5234/api"
+)
+
+$ErrorActionPreference = "Stop"
+
+if (Test-Path ".\src\client\.env") {
+    Remove-Item ".\src\client\.env" -Verbose
+}
+
+New-Item -ItemType File -Path ".\src\client\.env" -Force
+Set-Content -Path ".\src\client\.env" -Value $backendUrl
